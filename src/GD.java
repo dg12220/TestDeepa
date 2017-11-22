a69c5ed8dd42fec5e3706183f23d63f8954123d4
DS00359937 <DS00359937@INMUCHLP02420.TechMahindra.com>
2017-11-22 17:07:00 +0530

    gg

--- src/GD.java
+++ src/GD.java
@@ -1,1 +1,18 @@
-p
+
+public class GD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My name is Dhananjay su Gatade");
+		
+
+	}
+
+
+
+		
+		
+		
+	}
+
